eec9e47dafe9cf9c2a2ce681a82cd694fff4896d
TM-Consulting <mounaim.touinssi@gmail.com>
2022-01-01 02:23:29 +0100

    add statiscic container

--- data/covid-tracker.js
+++ data/covid-tracker.js
@@ -1,40 +1,125 @@
 const covidDataCities = {
   usa: {
-    cibledPeople: 10000000,
-    doses: {
-      dose_1: { daily: 3000, total: 14000 },
-      dose_2: { daily: 1000, total: 7000 },
+    newYork: {
+      cibledPeople: 10000000,
+      doses: [
+        {
+          title: "Daily – first dose",
+          amt: Math.floor(Math.random() * 1300) + 2000,
+        },
+        {
+          title: "Daily – second dose",
+          amt: Math.floor(Math.random() * 1300) + 2000,
+        },
+        {
+          title: "Total – first dose",
+          amt: Math.floor(Math.random() * 1300) + 20000,
+        },
+        {
+          title: "Total – second dose",
+          amt: Math.floor(Math.random() * 1300) + 20000,
+        },
+      ],
+    },
+    default: {
+      cibledPeople: 10000000,
+      doses: [
+        {
+          title: "Daily – first dose",
+          amt: Math.floor(Math.random() * 1300) + 2000,
+        },
+        {
+          title: "Daily – second dose",
+          amt: Math.floor(Math.random() * 1300) + 2000,
+        },
+        {
+          title: "Total – first dose",
+          amt: Math.floor(Math.random() * 1300) + 20000,
+        },
+        {
+          title: "Total – second dose",
+          amt: Math.floor(Math.random() * 1300) + 20000,
+        },
+      ],
     },
   },
 };
 const covidDataStats = {
   usa: { ...covidDataCities.usa },
 };
+const MONTHS = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
 
 const dataTracks = {
   usa: {
     newYork: [
       {
-        title: "hello world",
+        title: "People tested positive  - New York",
+        color: "#FBECEC",
+        lineDataKey: "positive",
+        data: new Array(12).fill({}).map((value, index) => {
+          return {
+            name: `${MONTHS[index]}`,
+            positive: Math.floor(Math.random() * 1300) + 2000,
+          };
+        }),
+      },
+      {
+        title: "Virus tests conducted - New York",
+        color: "#E5FBC1",
+        lineDataKey: "conducted",
+        data: new Array(12).fill({}).map((value, index) => {
+          return {
+            name: `${MONTHS[index]}`,
+            conducted: Math.floor(Math.random() * 1300) + 2000,
+          };
+        }),
+      },
+      {
+        title: "Deaths within 12 months of positive test - New York",
         color: "#FBECEC",
         lineDataKey: "death",
         data: new Array(12).fill({}).map((value, index) => {
           return {
-            name: `item_${index}`,
-            death: Math.floor(Math.random() * 16) + 5,
+            name: `${MONTHS[index]}`,
+            death: Math.floor(Math.random() * 1300) + 2000,
+          };
+        }),
+      },
+
+      {
+        title: "Patients admitted - New York",
+        color: "#E5FBC1",
+        lineDataKey: "admitted",
+        data: new Array(12).fill({}).map((value, index) => {
+          return {
+            name: `${MONTHS[index]}`,
+            admitted: Math.floor(Math.random() * 1300) + 2000,
           };
         }),
       },
     ],
     default: [
       {
-        title: "hello world default",
+        title: "People tested positive USA",
         color: "#FBECEC",
-        lineDataKey: "death",
+        lineDataKey: "positive",
         data: new Array(12).fill({}).map((value, index) => {
           return {
-            name: `item_${index}`,
-            death: Math.floor(Math.random() * 16) + 5,
+            name: `${MONTHS[index]}`,
+            positive: Math.floor(Math.random() * 1300) + 2000,
           };
         }),
       },
--- services/tracker.service.js
+++ services/tracker.service.js
@@ -1,6 +1,8 @@
 const covidData = require("../data/covid-tracker");
 const getStatistics = (req) => {
-  return covidData.covidDataStats[req.query.country];
+  return covidData.covidDataStats[req.query.country][
+    req.query.city ?? "default"
+  ];
 };
 const getTracks = (req) => {
   return covidData.covidDataTracks[req.query.country][
